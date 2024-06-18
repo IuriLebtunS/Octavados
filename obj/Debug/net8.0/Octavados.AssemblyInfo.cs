edb71e7fcc9e56e49a222bd81e519d62aa384166
Iuri Lebtun Salinas <Lebtuniuri@gmail.com>
2024-06-18 11:11:05 -0300

    ultimos Comit

--- obj/Debug/net8.0/Octavados.AssemblyInfo.cs
+++ obj/Debug/net8.0/Octavados.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Octavados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf4cf50af5b3e45fdad85de57273d7a5c0e3c13d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466c6c0856942dd2f3049730dcd075af81978793")]
 [assembly: System.Reflection.AssemblyProductAttribute("Octavados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Octavados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
